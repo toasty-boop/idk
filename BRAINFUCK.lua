600fe0ee7045eda63d6d4b6fdc02775152855e2b
sealtastic <tiranosaurio2604@gmail.com>
2026-02-13 19:18:44 -0600

    672B to 617B
    woo

--- BRAINFUCK.lua
+++ BRAINFUCK.lua
@@ -1,7 +1,8 @@
-q,z={},(0)q.n=function(b)p,x,s,m,r,h,w,a,u=z,z,string.split(b,""),{},"",z,string.byte,math.abs,0x1
-repeat x+=(0x2-u+z)i=w(s[x])if(a((0x2c)-i)-u)*(i-0x2e)*(a(0x3d-i)-u)*(a(0x5c-i)-u)==(z)then
-if(i<0x2e)then if(m[p])and((m[p]>0xff)or(m[p]<z))then end m[p]=(m[p]and(m[p]-(i-0x2c))or(0x2c-i))%(0x100)elseif
-(i<0x2f)then r=r..string.char(m[p])elseif(i<0x3f)then if(p<z)then error''return end p+=(i-0x3d)else if(i<0x5c)then
-t=(h)if(m[p]~=z)then h+=(u)else while(t>=h)do i=w(s[x])x+=(u)if(a(0x5c-i)==u)then t+=(i-0x5c)end
-end h=(t)end else t=(h+u)if(m[p]~=z)then repeat i=w(s[x-u])x-=(u)if(a(0x5c-i)==u)then t+=(i-0x5c)end
+q={}q.n=function(b)p,x,s,m,r,h,w,a=0,0,string.split(b,""),{},"",0,string.byte,math.abs
+repeat x+=(0x2-1+0)i=w(s[x])if(a(44-i)-1)*(i-46)*(a(61-i)-1)*(a(92-i)-1)==(0)then if(i<46)then
+if(m[p])and((m[p]>255)or(m[p]<0))then end
+m[p]=(m[p]and(m[p]-(i-44))or(44-i))%(256)elseif(i<47)then r=r..string.char(m[p])elseif(i<0x3f)then
+if(p<0)then return end p+=(i-61)else if(i<92)then
+t=(h)if(m[p]~=0)then h+=1 else while(t>=h)do i=w(s[x])x+=(1)if(a(92-i)==1)then t+=(i-92)end
+end h=(t)end else t=(h+1)if(m[p]~=0)then repeat i=w(s[x-1])x-=(1)if(a(92-i)==1)then t+=(i-92)end
 until t==(h)end end end end until(x>=#s)return(r)end return(q)
