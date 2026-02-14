3f3604122edbd1cdbbf2454c3a4fcbcfcd80a257
sealtastic <tiranosaurio2604@gmail.com>
2026-02-14 11:56:03 -0600

    569B to 561B

--- BRAINFUCK.lua
+++ BRAINFUCK.lua
@@ -1,7 +1,7 @@
-q={}q.n=function(b)p,x,s,m,r,h,w,a=0,0,string.split(b,""),{},"",0,string.byte,math.abs
-repeat x+=(1)i=w(s[x])if(a(44-i)-1)*(i-46)*(a(61-i)-1)*(a(92-i)-1)==(0)then if(i<46)then
+q={}q.n=function(b)p,x,s,m,r,h,w,a,n=0,0,string.split(b,""),{},"",0,string.byte,math.abs,92
+repeat x+=1 i=w(s[x])if(a(44-i)-1)*(i-46)*(a(61-i)-1)*(a(n-i)-1)==0 then if(i<46)then
 m[p]=(m[p]and(m[p]-(i-44))or(44-i))%(256)elseif(i<47)then r=r..string.char(m[p])elseif(i<63)then
-if(p<0)then return end p+=i-61 else if(i<92)then t=h if(m[p]~=0)then
-h+=1 else repeat i=w(s[x])x+=1 if(a(92-i)==1)then t+=(i-92)end until t<h h=(t)
-end else t=h+1 if(m[p]~=0)then repeat i=w(s[x-1])x-=1 if(a(92-i)==1)then
-t+=i-92 end until t==(h)end end end end until(x>=#s)return(r)end return(q)
+if(p<0)then return end p+=i-61 else if(i<n)then t=h if(m[p]~=0)then
+h+=1 else repeat i=w(s[x])x+=1 if(a(n-i)==1)then t+=i-n end until(t<h)h=t
+end else t=h+1 if(m[p]~=0)then repeat i=w(s[x-1])x-=1 if(a(n-i)==1)then
+t+=i-n end until t==h end end end end until(x>=#s)return(r)end return q
