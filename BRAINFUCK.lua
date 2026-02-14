00a8e71c677f01e2ffb1e4aca395f40a8ac3ff79
sealtastic <tiranosaurio2604@gmail.com>
2026-02-13 19:35:43 -0600

    579B to 576B

--- BRAINFUCK.lua
+++ BRAINFUCK.lua
@@ -1,7 +1,7 @@
 q={}q.n=function(b)p,x,s,m,r,h,w,a=0,0,string.split(b,""),{},"",0,string.byte,math.abs
 repeat x+=(2-1+0)i=w(s[x])if(a(44-i)-1)*(i-46)*(a(61-i)-1)*(a(92-i)-1)==(0)then if(i<46)then
 m[p]=(m[p]and(m[p]-(i-44))or(44-i))%(256)elseif(i<47)then r=r..string.char(m[p])elseif(i<63)then
-if(p<0)then return end p+=i-61 else if(i<92)then t=h if (m[p]~=0)then 
+if(p<0)then return end p+=i-61 else if(i<92)then t=h if m[p]~=0 then
 h+=1 else repeat i=w(s[x])x+=(1)if(a(92-i)==1)then t+=(i-92)end until t<h h=(t)
 end else t=(h+1)if(m[p]~=0)then repeat i=w(s[x-1])x-=(1)if(a(92-i)==1)then
 t+=(i-92)end until t==(h)end end end end
